71c4e87fbd4976ea9fd7026df4500691bebfedba
Matheus Fernando Ribeiro Martins <matheus1030br@gmail.com>
2025-09-29 11:54:43 -0300

    Arquivo editar

--- src/editar/curriculo.js
+++ src/editar/curriculo.js
@@ -1,2 +1,36 @@
 /*Aqui vamos inserir todos os dados e editra caso precise alterar algo no site, os pra fazer algo em que se edita em um só lugar e ja altera tudo*/
 
+import projetos from "../data/projetos";
+
+function curriculo(){
+    return{
+        nome: "Matheus Fernando Ribeiro Martins",
+        apelido: "Matheus Fernando",
+        dataNascimento: "27-08-2004",
+        cidade: "Minas Gerais",
+        estado: "MG",
+        cargo: "Analista de TI",
+        textoInicialHome: "testeTexto",
+        habilidades: [],
+        evoluicaoProfissional: [],
+        oqueDizemSobreMim: [],
+        minhasConquistas: [],
+        textoSobreMim: "testetexto",
+        cnh : "AB",
+        idioma: "Inglês - Básico",
+        regiao: "Timóteo - MG",
+        projetos: [],
+        cursos: [],
+        educacao: [],
+        competencias: [],
+        experiencia: [],
+        linkedin: "https://www.linkedin.com/in/matheus-ribeiro-7b4a9822b/",
+        github: "https://github.com/Matheus-Fernando-br",
+        telegram: "https://t.me/matheus1030br",
+        telefone: 31986763652,
+        whatsapp: 31986763652,
+        email: "matheus1030br@gmail.com",
+        curriculo: "https://drive.google.com/file/d/1-1Yk3gk2Yc0XH5yGmJH1b8rX4F0F3bXn/view?usp=sharing"
+    };
+}
+export default curriculo;
